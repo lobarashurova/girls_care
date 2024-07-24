b0d5ed5388c853eae2db1ba75190f2db4defa07d
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2024-07-24 23:25:37 +0500

    some comment

---
+++ lib/presentation/app/file_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FileWidget extends StatelessWidget {
+  const FileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
